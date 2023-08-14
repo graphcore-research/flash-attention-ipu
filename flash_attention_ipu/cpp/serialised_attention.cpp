57e170117e68fa481e005219f0a9c88c0219ed1c
Luke Prince <lukep@graphcore.ai>
2023-08-14 16:35:56 +0000

    vanilla attention grad

--- flash_attention_ipu/cpp/serialised_attention.cpp
+++ flash_attention_ipu/cpp/serialised_attention.cpp
@@ -185,6 +185,62 @@ poplar::Tensor vanillaAttention(
     return out;
 }
 
+poplar::Tensor vanillaAttentionBackward(
+    poplar::Graph& graph,
+    const poplar::Tensor& qkv, // shape 3 x G x L x D
+    const poplar::Tensor& grad, // shape G x L x D
+    poplar::program::Sequence& prog,
+    poplar::DebugContext& dc
+) {
+    assert(qkv.dim(0) == 3);
+    auto query = qkv[0];
+    auto key = qkv[1];
+    auto value = qkv[2];
+
+    /* 
+    Recompute attention 
+    */
+
+    // q @ k.T + mask
+    auto attn = poplin::matMulGrouped(graph, query, key.dimShuffle({0, 2, 1}), prog, query.elementType(), {dc, "attn = Q@K.T"});
+    
+    // generate causal masks
+    // clone attention matrix to colocate mask elements with attn matrix elements
+    auto mask = graph.clone(attn.elementType(), attn[0], {dc, "mask = array_like(attn[0])"});
+    popops::fill(graph, mask, prog, -10000.0, {dc, "fill(mask, -10000)"});
+    triu(graph, mask, 1, prog, {dc, "triu(mask)"});
+    popops::addInPlace(graph, attn, mask.expand({0}), prog, {dc, "attn += mask"});
+
+    // Stable softmax(x)
+    auto m = popops::reduce(graph, attn, attn.elementType(), {2}, {popops::Operation::MAX}, prog, {dc, "m = max(attn, dim=2)"});
+    popops::subInPlace(graph, attn, m.expand({2}), prog, {dc, "attn -= m"});
+    popops::expInPlace(graph, attn, prog, {dc, "attn = exp(attn)"});
+    auto s = popops::reduce(graph, attn, attn.elementType(), {2}, {popops::Operation::ADD}, prog, {dc, "s = sum(attn, dim=2)"});
+    popops::divInPlace(graph, attn, s.expand({2}), prog, {dc, "attn /= s"});
+
+    // Gradients!
+
+    // grad_v
+    auto value_grad = poplin::matMulGrouped(graph, attn.dimShuffle({0, 2, 1}), grad, prog, attn.elementType(), {dc, "dV = attn.T@dO"});
+    auto attn_grad = poplin::matMulGrouped(graph, grad, value.dimShuffle({0, 2, 1}), prog, grad.elementType(), {dc, "dattn = dO@V.T"});
+    
+    // softmax grad
+    popops::mulInPlace(graph, attn_grad, attn, prog, {dc, "dattn *= attn"});
+    s = popops::reduce(graph, attn_grad, attn_grad.elementType(), {2}, {popops::Operation::ADD}, prog, {dc, "s = sum(attn_grad, dim=2)"});
+    popops::mulInPlace(graph, attn, s.expand({2}), prog, {dc, "attn *= s"});
+    popops::subInPlace(graph, attn_grad, attn, prog, {dc, "dattn -= attn"});
+
+    // grad_q
+    auto query_grad = poplin::matMulGrouped(graph, attn_grad, key, prog, attn_grad.elementType(), {dc, "dQ = dattn @ K"});
+    // grad_k
+    auto key_grad = poplin::matMulGrouped(graph, attn_grad.dimShuffle({0, 2, 1}), query, prog, attn_grad.elementType(), {dc, "dK = dattn.T @ Q"});
+
+    // concat
+    auto qkv_grad = poplar::concat({query_grad.expand({0}), key_grad.expand({0}), value_grad.expand({0})}, 0);
+    
+    return qkv_grad;
+}
+
 poplar::Tensor serialisedAttention(
     poplar::Graph& graph, 
     const poplar::Tensor& qkv,  // Shape 3 x G x L x D
