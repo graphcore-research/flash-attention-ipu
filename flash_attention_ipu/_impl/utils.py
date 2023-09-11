110a609c091ab92a447f1c0d772cb25f123d5c61
Luke Yuri Prince <lyprince@users.noreply.github.com>
2023-09-11 10:38:43 +0100

    Patch toggle (#11)
    * patch when .auto imported
    
    * remove test main

--- flash_attention_ipu/_impl/utils.py
+++ flash_attention_ipu/_impl/utils.py
@@ -1,14 +1,19 @@
 from functools import wraps
 from typing import Any, Callable, List
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def patch_function(orig_fn: Callable, modules_to_patch: List[Any]):
     """Decorator util helping patching any Python function.
-    A simple example of patching a Numpy function (or JAX):
+    A simple example of patching a numpy function:
+    
     @patch_function(numpy.sin, [numpy])
     def noisy_sin(orig_sin, x):
-      print('sining!')
-      return orig_sin(x)
+        print('sining!')
+        return orig_sin(x)
+    
     Note that the first argument has to be the original function being patched, to
     avoid some kind of circular dependency in the implementation.
     Args:
@@ -21,11 +26,11 @@ def patch_function(orig_fn: Callable, modules_to_patch: List[Any]):
     def decorator_patch_fn(patched_fn: Callable):
         @wraps(orig_fn)
         def patch_wrapper(*args, **kwargs):
-            print(f"flash_attention_ipu: patching {orig_fn.__name__}!")
             return patched_fn(orig_fn, *args, **kwargs)
 
         fn_name = orig_fn.__name__
         for m in modules_to_patch:
+            logger.info(f"flash_attention_ipu: patching {orig_fn.__name__}!")
             setattr(m, fn_name, patch_wrapper)
         return patch_wrapper
 
